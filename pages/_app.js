48bb3edef16ef51e781774a12f5195f9fa1b23cc
noritakasawada <snp.inc.info@gmail.com>
2023-04-28 14:46:31 +0900

    test2

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import {LiffProvider} from "use-line-liff"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+   <LiffProvider liffId={process.env.NEXT_PUBLIC_LIFF_ID} mock={{enable:true}} >
+    <Component {...pageProps} />
+   </LiffProvider>
+  )
 }
 
 export default MyApp
